392ff282484d7292aa9dca5cfd13c1e7e7332a18
Markus Andresen <markua@stud.ntnu.no>
2016-03-02 13:48:55 +0100

    small changes to Client.py

--- KTN1/skeletoncode/Client.py
+++ KTN1/skeletoncode/Client.py
@@ -3,46 +3,46 @@ import socket
 from MessageReceiver import MessageReceiver
 from MessageParser import MessageParser
 
+
+# This is the chat client class
 class Client:
-    """
-    This is the chat client class
-    """
 
+    server_port = 0
+    host = None
+    running = None
+    socket = None
+
+    # This method is run when creating a new Client object
     def __init__(self, host, server_port):
-        """
-        This method is run when creating a new Client object
-        """
-
         # Set up the socket connection to the server
-        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
         # TODO: Finish init process with necessary code
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_port = server_port
+        self.host = host
+        self.running = True
         self.run()
 
     def run(self):
         # Initiate the connection to the server
         self.connection.connect((self.host, self.server_port))
-        
+
     def disconnect(self):
         # TODO: Handle disconnection
+        self.running = False
         pass
 
     def receive_message(self, message):
         # TODO: Handle incoming message
+        messageReciver = MessageReceiver(client, self.connection)
         pass
 
     def send_payload(self, data):
         # TODO: Handle sending of a payload
         pass
-        
-    # More methods may be needed!
+
+        # More methods may be needed!
 
 
+# This is the main method and is executed when you type "python Client.py"
 if __name__ == '__main__':
-    """
-    This is the main method and is executed when you type "python Client.py"
-    in your terminal.
-
-    No alterations are necessary
-    """
     client = Client('localhost', 9998)
