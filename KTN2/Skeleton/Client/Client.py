79fc9252ef874cd8edb0cc85e67e78ab4c5df5b6
Markus Andresen <markua@stud.ntnu.no>
2016-04-04 13:38:41 +0200

    refactor

--- KTN2/Skeleton/Client/Client.py
+++ KTN2/Skeleton/Client/Client.py
@@ -1,18 +1,14 @@
-# -*- coding: utf-8 -*-
 import socket
 from ClientMessageParser import MessageParser
 import threading
 import json
 
+# Client Class
 class Client:
-    '''
-    This is the chat client class
-    '''
 
+    # Constructor runs when creating a new Client object
     def __init__(self, host, server_port):
-        '''
-        This method is run when creating a new Client object
-        '''
+
         self.valid_reqs = ['login', 'logout', 'msg', 'names', 'help']
         self.host = host
         self.server_port = server_port
@@ -37,17 +33,16 @@ class Client:
             #self.send_payload()
             pass
 
-        
+
+    # close() releases the resource associated with a connection but does not necessarily close the connection
+    # immediately. If you want to close the connection in a timely fashion, call shutdown() before close(). - python docs.
     def disconnect(self):
-        # close() releases the resource associated with a connection but does not necessarily close the connection
-        # immediately. If you want to close the connection in a timely fashion, call shutdown() before close(). - python docs.
         self.connection.shutdown()
         self.connection.close()
 
     def receive_message(self):
         while 1:
             message = self.connection.recv(4096)
-            #print message
             print self.parser.parse(message)
 
     def valid_payload(self, req):
@@ -58,22 +53,17 @@ class Client:
             req = raw_input("Request: ").lower()
             while not self.valid_payload(req):
                 req = raw_input("Request: ").lower()
-            message_to_send = {'request': req, 'content': None}
+            msg_to_send = {'request': req, 'content': None}
             # Add content to payload if required
             if req == "login" or req == "msg":
                 content = raw_input("Content: ")
-                message_to_send['content'] = content
+                msg_to_send['content'] = content
             # Json-dump the string for standard JSON-formatted string
-            self.connection.send(json.dumps(message_to_send))
+            self.connection.send(json.dumps(msg_to_send))
             # Let the console have time to output response from server, find better solution?
             threading._sleep(0.5)
 
 
+# Main method
 if __name__ == '__main__':
-    '''
-    This is the main method and is executed when you type 'python Client.py'
-    in your terminal.
-
-    No alterations are necessary
-    '''
     client = Client("localhost", 9998)
